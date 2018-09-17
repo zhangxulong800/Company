90a72f1a55bdc1243860377b9713bc820cc45b29
longyiping <1203379811@qq.com>
2018-09-17 17:42:35 +0800

    5
    5

--- program/mall/commission.php
+++ program/mall/commission.php
@@ -1,29 +1,39 @@
 <?php
-$sql="select `id`,`actual_money`,`receipt_time` from ".self::$table_pre."order where `state`=6 and `buyer`='".$_SESSION['monxin']['username']."'";
-print_r($sql);exit;
+$sql="select `id`,`actual_money`,`receipt_time`,`goods_cost`,`is_add` from ".self::$table_pre."order where `state`=6 and `is_add`=0 and `buyer`='".$_SESSION['monxin']['username']."'";
 $userorder=$pdo->query($sql,2);
-$achievement=0;//自身消费业绩总数 ；系统一笔订单几款要退货都退
+$achievement=0;//本次自身消费业绩总数 ；系统一笔订单几款要退货都退;商品、会员卡类合在一起
 foreach($userorder as $val){
 	if((time()-$val['receipt_time'])/86400>=self::$config['refund_time_limit']){
-		print_r($val);exit;
-		$sql="select `price`,`goods_id`,`quantity` from ".self::$table_pre."order_goods where `order_id`=".$val['id'];//测试price是单价
-		$r=$pdo->query($sql,2);
-		//判断商品是否是会员卡类
 		$achievement+=$val['actual_money'];
 	}
 }
-//区分会员卡和购物两种情形计算
-//计算直属会员总数
-$sum_sql="select count(id) as c  from ".$pdo->index_pre."user where `state`=1 and `pid`='".$_SESSION['monxin']['id']."'";
-$once=$pdo->query($sum_sql,2)->fetch(2);
-$sum=$once['c'];
-//print_r($sum);exit;
-//变更会员业绩、级别 （级别：0会员2经理5总监）
-if($sum>=30 && $achievement>=10000){
-	$grade=2;
-} else if($sum>=50 && $achievement>=300000){
-	$grade=5;
+if($achievement>0){
+	//获取会员对应的信息
+	$sql="select `id`,`achievement`,`path_pid` from ".$pdo->index_pre."user where `id`='".$_SESSION['monxin']['id']."'";
+	$nowuser=$pdo->query($sql,2)->fetch(2);
+	$achievement+=$nowuser['achievement'];
+	//计算直属会员总数
+	$sum_sql="select count(id) as c  from ".$pdo->index_pre."user where `state`=1 and `pid`='".$_SESSION['monxin']['id']."'";
+	$once=$pdo->query($sum_sql,2)->fetch(2);
+	$sum=$once['c'];
+	//变更会员业绩、级别 （级别：0会员2经理5总监）
+	if($sum>=30 && $achievement>=10000){
+		$grade=2;
+	} else if($sum>=50 && $achievement>=300000){
+		$grade=5;
+	}
+	$ngrade=empty($grade)?0:$grade;
+	$sql="update ".$pdo->index_pre."user set `achievement`=".$achievement.",`grade`=".$ngrade." where `id`=".$_SESSION['monxin']['id'];
+	$pdo->exec($sql);
+	//计算提成
+	$pid_arr=empty($nowuser['path_pid'])?array():explode('#',$nowuser['path_pid']);
+	$tot_money=0;
+	foreach($userorder as $val){
+		if((time()-$val['receipt_time'])/86400>=self::$config['refund_time_limit']){
+			$sql="update ".self::$table_pre."order set `is_add`=1 where `id`=".$val['id'];
+			$pdo->exec($sql);
+			//依据上级等级计算分成并将明细存入数据库，然后变更总数
+		}
+	}
+	print_r($pid_arr);exit;
 }
-$ngrade=empty($grade)?0:$grade;
-$sql="update ".$pdo->index_pre."user set `achievement`=".$achievement.",`grade`=".$ngrade." where `id`=".$_SESSION['monxin']['id'];
-$pdo->exec($sql);
--- z_readme.txt
+++ z_readme.txt
@@ -4,6 +4,6 @@
 商城数据显示类 示例 ./index.php?monxin=mall.goods_add (monxin=类名.方法名)，
 大部分情况是通过 __call方法 加载执行 ./program/mall/show/ 目录下的对应名称的文件
 /*----------*/
-首页楼层文件program\mall\show\goods_module.php
+首页楼层文件program\mall\show\goods_module.php 价格显示的代码在该页面中。
 /*-----自动收货-----*/
 <script src="./receive.php?target=mall::auto_receipt_expire_order"></script>
